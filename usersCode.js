cb01450089c6374f3ac5993ee5d4d10d9628b923
Jason Tran <jasontran.pk@gmail.com>
2022-12-27 19:47:01 +0700

    fix writejson in usercode

--- usersCode.js
+++ usersCode.js
@@ -2,10 +2,11 @@ const { processVersions } = require('./solution');
 const { dummyTasks: inputData } = require('./dummy-input');
 const fse = require('fs-extra');
 
-const { data, error } = processVersions(inputData);
+const RESULT_PATH = './result_in_json.json';
 
-if (!error) {
-	fse.writeJSONSync(data);
-} else {
-	console.log(error);
-}
+processVersions(inputData)
+	.then((result) => {
+		console.log(result);
+		fse.writeJSONSync(RESULT_PATH, result.data);
+	})
+	.catch((err) => console.log(err));
